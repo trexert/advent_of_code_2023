a60c5f11b5260da6ea46c8527c737e2a6fffe6b7
trexert <starver@hotmail.co.uk>
2023-12-01 13:38:38 +0000

    Make a mess of day 1

--- aoc2023/Program.cs
+++ aoc2023/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] cliArgs = Environment.GetCommandLineArgs();
+if (cliArgs.Length != 2)
+{
+    Console.WriteLine("Usage: dotnet run <day>");
+    return;
+}
+
+int day = int.Parse(cliArgs[1]);
+
+(string part1, string? part2) = day switch
+{
+    1 => (Day1.Part1(), Day1.Part2()),
+    _ => throw new NotImplementedException($"Day {day} is not implemented yet"),
+};
+
+Console.WriteLine($"Day{day} part1 - {part1}, part2 - {part2}");
